b6eeb6660f0d303e7a295ead595c90c7a2459d4e
j0nas <jonas.jensen@msn.com>
2014-12-31 03:09:44 +0100

    moved navbar + jumbotron -> layout.jade fixed paths (favicon, createaccount)

--- app.js
+++ app.js
@@ -124,6 +124,10 @@ app.get("/users", function (req, res, next) {
     });
 });
 
+app.get("/users/new", function (req, res, next) {
+    res.render('createaccount');
+});
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
