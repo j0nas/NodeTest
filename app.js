c1879fdd547c074abbbdc36102c1b7a53f502dc6
j0nas <jonas.jensen@msn.com>
2015-01-26 15:55:08 +0100

    Bower-ified everything! bootstrap, angular, jquery now local.

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use(expressSession({
     secret: 'EdYouSessionToken',
